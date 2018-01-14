ce390f7d6cbfc41e4271003909730478382ee603
Ben Sparkes <bensparkes8@gmail.com>
2018-01-14 17:20:25 +0000

    Added iPhone8,4 10.3.2b offsets

--- Meridian/Meridian/offsets.m
+++ Meridian/Meridian/offsets.m
@@ -4937,6 +4937,28 @@ int load_offsets(void)
             
             
         }
+        // 10.3.2 Beta
+        if (!strcmp(osversion, "14F5065b"))
+        {
+            OFFSET_ZONE_MAP                        = 0xfffffff007548478;
+            OFFSET_KERNEL_MAP                      = 0xfffffff0075a4050;
+            OFFSET_KERNEL_TASK                     = 0xfffffff0075a4048;
+            OFFSET_REALHOST                        = 0xfffffff00752aba0;
+            OFFSET_BZERO                           = 0xfffffff007081f80;
+            OFFSET_BCOPY                           = 0xfffffff007081dc0;
+            OFFSET_COPYIN                          = 0xfffffff0071806f4;
+            OFFSET_COPYOUT                         = 0xfffffff0071808e8;
+            OFFSET_ROOTVNODE                       = 0xfffffff0075a40b0;
+            OFFSET_CHGPROCCNT                      = 0xfffffff00738d944;
+            OFFSET_KAUTH_CRED_REF                  = 0xfffffff007367cf4;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          = 0xfffffff007099e94;
+            OFFSET_IPC_KOBJECT_SET                 = 0xfffffff0070ad16c;
+            OFFSET_IPC_PORT_MAKE_SEND              = 0xfffffff0070999b8;
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    = 0xfffffff006e849f8;
+            OFFSET_ROP_ADD_X0_X0_0x10              = 0xfffffff006482174;
+            OFFSET_OSSERIALIZER_SERIALIZE          = 0xfffffff007440ad0;
+            OFFSET_ROP_LDR_X0_X0_0x10              = 0xfffffff0063d0a84;
+        }
         // 10.3.1
         if(!strcmp(osversion, "14E304"))
         {
