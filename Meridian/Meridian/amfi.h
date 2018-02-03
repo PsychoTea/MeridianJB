be59439904214221a422d742f1dc5dd396560d38
Ben Sparkes <bensparkes8@gmail.com>
2018-02-03 00:47:39 +0000

    get_code_dir w/ FAT + warnings

--- Meridian/Meridian/amfi.h
+++ Meridian/Meridian/amfi.h
@@ -6,12 +6,12 @@
 //  Copyright © 2017 Ben Sparkes. All rights reserved.
 //
 
-void init_amfi();
+void init_amfi(void);
 int defecate_amfi(void);
 int patch_amfi(mach_port_t amfi_port);
 int get_kqueue_for_pid(pid_t pid);
 void inject_trust(const char *path);
 
-uint8_t *get_code_directory(const char* name);
+uint8_t *get_code_directory(const char* file_path, uint64_t file_off);
 uint8_t *get_sha1(uint8_t* code_dir);
 uint32_t swap_uint32(uint32_t val);
