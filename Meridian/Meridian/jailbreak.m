931a8fe47c798157e886b29c3e0bc2f5c1cf9cc8
Ben Sparkes <bensparkes8@gmail.com>
2018-03-07 20:40:35 +0000

    [Jailbreak] Fix /meridian/tar not being created

--- Meridian/Meridian/jailbreak.m
+++ Meridian/Meridian/jailbreak.m
@@ -113,6 +113,9 @@ int makeShitHappen(ViewController *view) {
     }
     [view writeText:@"done!"];
     
+    mkdir("/meridian", 0755);
+    extract_bundle("tar.tar", "/meridian");
+    
     // extract meridian-base
     [view writeText:@"extracting meridian files..."];
     ret = extractMeridianData();
