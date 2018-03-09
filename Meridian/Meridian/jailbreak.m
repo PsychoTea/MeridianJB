30e74667197dc1f4b8f68da126889240c221bcb5
Sticktron <sticktron@hotmail.com>
2018-03-09 09:13:11 -0500

    file_exist() -> file_exists()

--- Meridian/Meridian/jailbreak.m
+++ Meridian/Meridian/jailbreak.m
@@ -80,13 +80,17 @@ int makeShitHappen(ViewController *view) {
 
     // run v0rtex
     [view writeText:@"running v0rtex..."];
-    // suspend_all_threads();
+    usleep(100000);
+    
+    suspend_all_threads();
     ret = runV0rtex();
+    resume_all_threads();
+    sleep(1);
+
     if (ret != 0) {
         [view writeText:@"failed!"];
         return 1;
     }
-    // resume_all_threads();
     [view writeTextPlain:@"succeeded! praize siguza!"];
     
     // set up stuff
@@ -402,7 +406,7 @@ int startJailbreakd() {
     int rv = start_launchdaemon("/meridian/jailbreakd/jailbreakd.plist");
     if (rv != 0) return 1;
     
-    while (!file_exist("/var/tmp/jailbreakd.pid")) {
+    while (!file_exists("/var/tmp/jailbreakd.pid")) {
         printf("Waiting for jailbreakd \n");
         usleep(300000); // 300ms
     }
