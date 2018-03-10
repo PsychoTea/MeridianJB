7fa8821ef1cfe8362d87d7ba108314b66c06bb72
Ben Sparkes <bensparkes8@gmail.com>
2018-03-10 21:06:25 +0000

    Update to `make`

--- Meridian/Meridian/bootstrap/create-meridian-base.sh
+++ Meridian/Meridian/bootstrap/create-meridian-base.sh
@@ -5,22 +5,19 @@ meridianDir=$currDir/../..
 baseDir=$currDir/meridian-base
 
 # amfid (fucker & payload)
-#mv $meridianDir/amfid/bin/* $baseDir/meridian/amfid
+cp $meridianDir/amfid/bin/* $baseDir/meridian/amfid
 
 # injector
-#mv $meridianDir/injector/bin/* $baseDir/meridian/
+cp $meridianDir/injector/bin/* $baseDir/meridian/
 
 # pspawn_hook.dylib
-#mv $meridianDir/pspawn_hook/bin/* $baseDir/meridian
-
-# libjailbreak.dylib
-#mv $meridianDir/libjailbreak/bin/* $baseDir/usr/lib
+cp $meridianDir/pspawn_hook/bin/* $baseDir/meridian
 
 # jailbreakd
-#mv $meridianDir/jailbreakd/bin/* $baseDir/meridian/jailbreakd
+cp $meridianDir/jailbreakd/bin/* $baseDir/meridian/jailbreakd
 
 # TweakLoader.dylib
-#mv $meridianDir/TweakLoader/bin/* $baseDir/usr/lib
+cp $meridianDir/TweakLoader/.theos/obj/debug/*.dylib $baseDir/usr/lib
 
 # remove all .DS_Store files
 find $baseDir -name '.DS_Store' -delete
