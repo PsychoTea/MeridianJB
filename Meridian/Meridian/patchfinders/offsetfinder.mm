ff7b8a337c08492ed0077be35af794ecd74ac22c
Ben Sparkes <bensparkes8@gmail.com>
2018-03-11 20:07:10 +0000

    [offsetfinder] try/catch

--- Meridian/Meridian/patchfinders/offsetfinder.mm
+++ Meridian/Meridian/patchfinders/offsetfinder.mm
@@ -8,6 +8,8 @@
 
 #include "v0rtex.h"
 #include "liboffsetfinder64.hpp"
+#include "ViewController.h"
+#import <Foundation/Foundation.h>
 
 static bool DidInit = false;
 static offsets_t off;
@@ -18,43 +20,51 @@ extern "C" offsets_t *get_offsets() {
     if (DidInit) {
         return &off;
     }
-    
-    tihmstar::offsetfinder64 fi("/System/Library/Caches/com.apple.kernelcaches/kernelcache");
-    
-    off.base = 0xfffffff007004000;
-    
-    off.sizeof_task                         = (kptr_t)fi.find_sizeof_task();
-    off.task_itk_self                       = (kptr_t)fi.find_task_itk_self();
-    off.task_itk_registered                 = (kptr_t)fi.find_task_itk_registered();
-    off.task_bsd_info                       = (kptr_t)fi.find_task_bsd_info();
-    off.proc_ucred                          = (kptr_t)fi.find_proc_ucred();
-    off.vm_map_hdr                          = (kptr_t)fi.find_vm_map_hdr();
-    off.ipc_space_is_task                   = (kptr_t)fi.find_ipc_space_is_task();
-    off.realhost_special                    = 0x10;
-    off.iouserclient_ipc                    = (kptr_t)fi.find_iouserclient_ipc();
-    off.vtab_get_retain_count               = (kptr_t)fi.find_vtab_get_retain_count();
-    off.vtab_get_external_trap_for_index    = (kptr_t)fi.find_vtab_get_external_trap_for_index();
-    
-    off.zone_map                            = (kptr_t)fi.find_zone_map();
-    off.kernel_map                          = (kptr_t)fi.find_kernel_map();
-    off.kernel_task                         = (kptr_t)fi.find_kernel_task();
-    off.realhost                            = (kptr_t)fi.find_realhost();
-    
-    off.copyin                              = (kptr_t)fi.find_copyin();
-    off.copyout                             = (kptr_t)fi.find_copyout();
-    off.chgproccnt                          = (kptr_t)fi.find_chgproccnt();
-    off.kauth_cred_ref                      = (kptr_t)fi.find_kauth_cred_ref();
-    off.ipc_port_alloc_special              = (kptr_t)fi.find_ipc_port_alloc_special();
-    off.ipc_kobject_set                     = (kptr_t)fi.find_ipc_kobject_set();
-    off.ipc_port_make_send                  = (kptr_t)fi.find_ipc_port_make_send();
-    off.osserializer_serialize              = (kptr_t)fi.find_osserializer_serialize();
-    off.rop_ldr_x0_x0_0x10                  = (kptr_t)fi.find_rop_ldr_x0_x0_0x10();
-    
-    offset_rootvnode                        = (kptr_t)fi.find_rootvnode();
-    offset_zonemap                          = off.zone_map;
-    
+
+    try {
+        tihmstar::offsetfinder64 fi("/System/Library/Caches/com.apple.kernelcaches/kernelcache");
+
+        off.base = 0xfffffff007004000;
+
+        off.sizeof_task                         = (kptr_t)fi.find_sizeof_task();
+        off.task_itk_self                       = (kptr_t)fi.find_task_itk_self();
+        off.task_itk_registered                 = (kptr_t)fi.find_task_itk_registered();
+        off.task_bsd_info                       = (kptr_t)fi.find_task_bsd_info();
+        off.proc_ucred                          = (kptr_t)fi.find_proc_ucred();
+        off.vm_map_hdr                          = (kptr_t)fi.find_vm_map_hdr();
+        off.ipc_space_is_task                   = (kptr_t)fi.find_ipc_space_is_task();
+        off.realhost_special                    = 0x10;
+        off.iouserclient_ipc                    = (kptr_t)fi.find_iouserclient_ipc();
+        off.vtab_get_retain_count               = (kptr_t)fi.find_vtab_get_retain_count();
+        off.vtab_get_external_trap_for_index    = (kptr_t)fi.find_vtab_get_external_trap_for_index();
+
+        off.zone_map                            = (kptr_t)fi.find_zone_map();
+        off.kernel_map                          = (kptr_t)fi.find_kernel_map();
+        off.kernel_task                         = (kptr_t)fi.find_kernel_task();
+        off.realhost                            = (kptr_t)fi.find_realhost();
+
+        off.copyin                              = (kptr_t)fi.find_copyin();
+        off.copyout                             = (kptr_t)fi.find_copyout();
+        off.chgproccnt                          = (kptr_t)fi.find_chgproccnt();
+        off.kauth_cred_ref                      = (kptr_t)fi.find_kauth_cred_ref();
+        off.ipc_port_alloc_special              = (kptr_t)fi.find_ipc_port_alloc_special();
+        off.ipc_kobject_set                     = (kptr_t)fi.find_ipc_kobject_set();
+        off.ipc_port_make_send                  = (kptr_t)fi.find_ipc_port_make_send();
+        off.osserializer_serialize              = (kptr_t)fi.find_osserializer_serialize();
+        off.rop_ldr_x0_x0_0x10                  = (kptr_t)fi.find_rop_ldr_x0_x0_0x10();
+
+        offset_rootvnode                        = (kptr_t)fi.find_rootvnode();
+        offset_zonemap                          = off.zone_map;
+    } catch (tihmstar::exception &e) {
+        NSLog(@"offsetfinder failure! %d (%s)", e.code(), e.what());
+        return NULL;
+    } catch (std::exception &e) {
+        NSLog(@"fatal offsetfinder failure! %s", e.what());
+        return NULL;
+    }
+        
     DidInit = true;
-    
+
     return &off;
 }
 
