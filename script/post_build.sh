bf2913f29748627030c6cf5a8eb50746ab938c08
Ben Sparkes <bensparkes8@gmail.com>
2018-03-12 21:45:24 +0000

    More travis

--- script/post_build.sh
+++ script/post_build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-ls
-pwd
-find ~/Library/Developer/Xcode/Archives
+cd Meridian
 
+xcodebuild -scheme Meridian -archivePath Meridian.xcarchive 
+
+xcodebuild -exportArchive -archivePath Meridian.xcarchive -exportPath Meridian.ipa -exportOptionsPlist exportPlist.plist
+
+rm -r Meridian.xcarchive
+
+ls -l Meridian.ipa
+echo "Generated ipa... where now?"
+echo "$(ls -l Meridian.ipa)"
