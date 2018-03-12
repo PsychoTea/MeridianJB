abfcfb5dbd7a6514d2c9f2351edce86b4abaf6f7
Ben Sparkes <bensparkes8@gmail.com>
2018-03-12 22:38:58 +0000

    ...sudo?

--- script/pre_build.sh
+++ script/pre_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Install ldid for post-build fakesigning (this is bad, sorry saurik)
-wget http://dl.sparko.me/ldid -O /usr/bin/ldid
+sudo wget http://dl.sparko.me/ldid -O /usr/bin/ldid
 chmod +x /usr/bin/ldid
