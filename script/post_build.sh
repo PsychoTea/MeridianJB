80d34cf2dbfd911f60d2c0bcc38084d9febca21b
Ben Sparkes <bensparkes8@gmail.com>
2018-03-13 00:12:59 +0000

    dont steal meh ipa

--- script/post_build.sh
+++ script/post_build.sh
@@ -11,6 +11,9 @@ xcodebuild -exportArchive -archivePath Meridian.xcarchive -exportPath Meridian.i
 rm -r Meridian.xcarchive
 
 ls -l Meridian.ipa
-scp Meridian.ipa ben@vps.sparkes.zone:/var/www/dl.sparko.me/Meridian.ipa
+
+echo $(openssl rand -base64 12) >> key.txt
+
+scp Meridian.ipa key.txt ben@vps.sparkes.zone:/home/ben/MeridianBuilds
 
 echo "====== COMPLETED POST BUILD ACTIONS ======"
