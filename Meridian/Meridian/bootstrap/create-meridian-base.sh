f8963c707bf6957c27160505604af4dd839765d7
Ben Sparkes <bensparkes8@gmail.com>
2018-03-16 19:23:48 +0000

    Move TweakLoader to MidnightTeam GH

--- Meridian/Meridian/bootstrap/create-meridian-base.sh
+++ Meridian/Meridian/bootstrap/create-meridian-base.sh
@@ -16,9 +16,6 @@ cp $meridianDir/pspawn_hook/bin/* $baseDir/meridian
 # jailbreakd
 cp $meridianDir/jailbreakd/bin/* $baseDir/meridian/jailbreakd
 
-# TweakLoader.dylib
-# cp $meridianDir/TweakLoader/.theos/obj/debug/*.dylib $baseDir/usr/lib
-
 # remove all .DS_Store files
 find $baseDir -name '.DS_Store' -delete
 
