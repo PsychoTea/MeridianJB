1f616b74e0fc6da1abbf8e69a8a1d7744330e818
Ben Sparkes <bensparkes8@gmail.com>
2018-03-17 13:26:29 +0000

    [jailbreak] Temp remove logging

--- Meridian/jailbreakd/main.m
+++ Meridian/jailbreakd/main.m
@@ -46,21 +46,21 @@ int handle_command(uint8_t command, uint32_t pid) {
         return 1;
     }
     
-    char *name = proc_name(pid);
+    // char *name = proc_name(pid);
     
     if (command == JAILBREAKD_COMMAND_ENTITLE) {
-        NSLog(@"JAILBREAKD_COMMAND_ENTITLE PID: %d NAME: %s", pid, name);
+        //NSLog(@"JAILBREAKD_COMMAND_ENTITLE PID: %d NAME: %s", pid, name);
         setcsflagsandplatformize(pid);
     }
     
     if (command == JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT) {
-        NSLog(@"JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT PID: %d NAME: %s", pid, name);
+        //NSLog(@"JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT PID: %d NAME: %s", pid, name);
         setcsflagsandplatformize(pid);
         kill(pid, SIGCONT);
     }
     
     if (command == JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT_FROM_XPCPROXY) {
-        NSLog(@"JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT_FROM_XPCPROXY PID: %d NAME: %s", pid, name);
+        //NSLog(@"JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT_FROM_XPCPROXY PID: %d NAME: %s", pid, name);
         __block int PID = pid;
         
         dispatch_queue_t queue = dispatch_queue_create("org.coolstar.jailbreakd.delayqueue", NULL);
@@ -81,17 +81,17 @@ int handle_command(uint8_t command, uint32_t pid) {
     }
     
     if (command == JAILBREAKD_COMMAND_FIXUP_SETUID) {
-        NSLog(@"JAILBREAKD_FIXUP_SETUID PID: %d NAME: %s", pid, name);
+        //NSLog(@"JAILBREAKD_FIXUP_SETUID PID: %d NAME: %s", pid, name);
         fixupsetuid(pid);
     }
     
-    free(name);
+    // free(name);
     
     return 0;
 }
 
 kern_return_t jbd_call(mach_port_t server_port, uint8_t command, uint32_t pid) {
-    NSLog(@"[Mach] New call from %x: command %x, pid %d", server_port, command, pid);
+    // NSLog(@"[Mach] New call from %x: command %x, pid %d", server_port, command, pid);
     return (handle_command(command, pid) == 0) ? KERN_SUCCESS : KERN_FAILURE;
 }
 
