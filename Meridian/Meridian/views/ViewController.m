3e3db3b1f3442c7ab21a07390c1be7f106f6c1f0
Ben Sparkes <bensparkes8@gmail.com>
2019-05-13 01:34:40 +0100

    [VC] Version bump

--- Meridian/Meridian/views/ViewController.m
+++ Meridian/Meridian/views/ViewController.m
@@ -25,7 +25,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *versionLabel;
 @end
 
-NSString *Version = @"Meridian: v0.9-007 Pre-Release";
+NSString *Version = @"Meridian: v0.9-008 Pre-Release";
 NSOperatingSystemVersion osVersion;
 
 id thisClass;
